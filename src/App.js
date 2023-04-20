faf46ce0cf9bd9fefc3daa65e6b164f29e3d5397
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2023-04-20 12:26:45 +0530

    contact app changes

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const renderLineChart = (
+  <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis dataKey="name" tick={renderCustomAxisTick} />
+    <YAxis />
+    <Tooltip />
+  </LineChart>
+);
